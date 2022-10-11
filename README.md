e138e266d2487690d94874184a4791d6d00e77db
KratosvsEzio <jadude19@gmail.com>
2022-10-11 15:37:55 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dynamic-end-sidebar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yez8mx)
---
+++ src/app/sidenav-position-example.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidenav-position-example',
+  templateUrl: 'sidenav-position-example.html',
+  styleUrls: ['sidenav-position-example.css'],
+})
+export class SidenavPositionExample {
+  public condition: boolean = true;
+}
